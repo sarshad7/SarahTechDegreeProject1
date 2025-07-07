b15c570a8ad453425a411d124f6a7219f8c715e9
sarshad7 <sarshad1576@gmail.com>
2025-07-06 22:12:15 -0400

    added quotes, sources, year, and citation properties to each object. Then, I created the getRandomQuote function.

--- js/script.js
+++ js/script.js
@@ -11,12 +11,41 @@ project 1 - A Random Quote Generator
  * `quotes` array
  ***/
 const quotes = [
-  //object 1
+  {
+    quote: "The wound is the place where the Light enters you.",
+    source: "Jalaluddin Rumi",
+    citation: "The Masnavi by Jalaluddin Rumi",
+    year: 1262,
+  },
+  {
+    quote:
+      "A little knowledge that acts is worth infinitely more than much knowledge that is idle.",
+    source: "Khalil Gibran",
+  },
+  {
+    quote: "I am the master of my fate. I am the captain of my soul.",
+    source: "Wlliam Ernest Henley",
+    year: 1875,
+  },
+  {
+    quote: "I am neither a flower of melody nor a veil of music.",
+    source: "Faiz Ahmad Faiz",
+  },
+  {
+    quote:
+      "“Failure is not fatal until we surrender. Trying again is the key of glorious victory.",
+    source: "Muhammad Iqbal",
+  },
 ];
 
 /***
  * `getRandomQuote` function
  ***/
+function getRandomQuote() {
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  const randomQuoteObject = quotes[randomNumber];
+  return randomQuoteObject;
+}
 
 /***
  * `printQuote` function
