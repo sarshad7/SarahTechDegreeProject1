91f523475a330be795472b2ecfa97e8873de0e20
sarshad7 <sarshad1576@gmail.com>
2025-07-15 22:46:27 -0400

    Added comments for readability

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ project 1 - A Random Quote Generator
 /***
  * `quotes` array
  ***/
+// Created a quotes array from a handful of my favorite authors and poets, with quote and source attached to each, and citation, year, tags in others.
 const quotes = [
   {
     quote: "The wound is the place where the Light enters you.",
@@ -42,12 +43,13 @@ const quotes = [
 /***
  * `getRandomQuote` function
  ***/
+// Created a function to store a value from 0 to the length of the quotes array in randomNumber. Then I used it as an index to the quotes array, which then stores in randomQuoteObject and returns the value.
 function getRandomQuote() {
   const randomNumber = Math.floor(Math.random() * quotes.length);
   const randomQuoteObject = quotes[randomNumber];
   return randomQuoteObject;
 }
-
+// Created a random color function to dictate the background color. Used the same math.random() method as before with 256 being the highest possible permutation, and stored those in red, green, and blue variables. The final line selects the body of the page, calls the style and background color, and changes to whatever the function generated.
 function changerandomColor() {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
@@ -56,11 +58,10 @@ function changerandomColor() {
   document.querySelector("body").style.backgroundColor = RGBcolor;
 }
 
-function quoteUpdate() {}
-
 /***
  * `printQuote` function
  ***/
+// Created a printQuote function to take the random quote which was stored from before, and insert it into elements within the page. The object in the quotes array will pass in the quote, source, citation/year/tags (if they exist) into the HTML element using template literals.
 function printQuote() {
   const randomQuote = getRandomQuote();
   let html = `<p class="quote">${randomQuote.quote}</p>`;
@@ -84,8 +85,9 @@ function printQuote() {
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
  ***/
-
+// Used the setInterval to call the printQuote function and run it every 10,000 milliseconds/10 seconds
 document
   .getElementById("load-quote")
   .addEventListener("click", printQuote, false);
 setInterval(printQuote, 10000);
+// I'm aiming for exceeds expectations, but I'm okay with meet expectations as the final grade.
